026a5ce6f2ee0b386215ebe85cc9f56047c2e73d
FelixC <felix.carvajal@evertecinc.com>
2021-07-22 00:30:38 -0400

    Creacion del Modelo de Productos

---
+++ ISSB.Web/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ISSB.Web.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        
+        [Required]
+        [Display(Name = "Nombre Producto")]
+        public string ProductName { get; set; }
+
+        [Display(Name = "Descripcion Producto")]
+        public string ProductDescription { get; set; }
+        
+        [Display(Name = "Precio")]
+        public decimal Price { get; set; }
+
+        public double Stock { get; set; }
+    }
+}
